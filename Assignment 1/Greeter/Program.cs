366dd68925ed2c61925e98d65ee82bf670440c91
Sadeek Ghimire <sadeekg133@gmail.com>
2024-03-03 21:26:06 +0545

    Updated

--- Assignment 1/Greeter/Program.cs
+++ Assignment 1/Greeter/Program.cs
@@ -1,4 +1,23 @@
 ﻿Console.Write("Enter the full name: ");
 string fullName= Console.ReadLine(); 
 string cFullName=fullName.ToUpper();
-Console.WriteLine($"Hello, {cFullName} Ji!");
+Console.Write("Your DOB (yyyy/mm/dd): ");
+DateTime.TryParse(Console.ReadLine(), out DateTime dob);
+var ageOutput=CalculateAge(dob);
+Console.WriteLine($"Hello, {cFullName} Ji! \nYour DOB: {dob.ToLongDateString()} \n{ageOutput}");
+string CalculateAge(DateTime dob){
+    // Calculate the age
+    TimeSpan difference = DateTime.Now - dob;
+    var days = difference.TotalDays;
+
+    var years = (int)(days / 365);
+    var remainingDays = days % 365;
+    var months =(int)(remainingDays / 30);
+   
+    var remainingDaysInMonth = (int)(remainingDays % 30);
+    var weeks=(int)(remainingDaysInMonth/7);
+    var remainingDaysInWeek=(int)(remainingDaysInMonth%7);
+
+    var output =$"Age as of now: {years} years, {months} months, {weeks} weeks and {remainingDaysInMonth} days.";
+    return output;
+}
--- Assignment 1/Greeter/obj/Debug/net8.0/Greeter.AssemblyInfo.cs
+++ Assignment 1/Greeter/obj/Debug/net8.0/Greeter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Greeter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d02d89c192f70c1a3989be2762161f37109153")]
 [assembly: System.Reflection.AssemblyProductAttribute("Greeter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Greeter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
