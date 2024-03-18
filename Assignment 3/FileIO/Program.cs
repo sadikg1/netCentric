8101c1b95bc19e477f4178dd2fced5d9095257d2
Sadeek Ghimire <sadeekg133@gmail.com>
2024-03-18 21:29:46 +0545

    update

---
+++ Assignment 3/FileIO/Program.cs
@@ -0,0 +1,9 @@
+﻿string filePath = @"E:\Semester\Sadik 6th Sem\NetCentric.Vedas2080\Assignment 3\Inflation.csv";
+
+// Instantiate the Inflation class
+Inflation inflation = new Inflation();
+
+// Call the method to read the CSV file and create properties dynamically
+inflation.SetPropertiesFromCSV(filePath);
+inflation.PrintProperties();
+Console.WriteLine("Successfully created properties");
